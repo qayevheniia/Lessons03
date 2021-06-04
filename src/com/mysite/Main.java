9ee05f074f6a410ea94d7d5621cb5d7900afc19d
Yevgeniia Motytskaya <qayevheniia@gmail.com>
2021-06-04 20:23:59 +0300

    lesson03

---
+++ src/com/mysite/Main.java
@@ -0,0 +1,32 @@
+package com.mysite;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner in= new Scanner(System.in);
+
+        //System.out.println("Please, enter your age and press Enter");
+        // int number= in.nextInt();
+        // int age = 18;
+        //
+        // if (number > age) {
+        //     System.out.println("Welcome");
+        // } else if (number == age) {
+        //     System.out.println("Sorry, wait a bit");
+        // } else {
+        //     System.out.println("Sorry, denied");
+        // }
+        //
+        // int target_age=20;
+        //
+        // String result = (age >= target_age) ? "Welcome": "Sorry, denied";
+        // System.out.println(result);
+
+
+    }
+    }
+
